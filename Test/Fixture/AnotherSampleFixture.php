aa2948c64119b1187f51915c4b15640edbaafdb4
Zuborawka <zuborawka@live.jp>
2012-12-13 06:36:31 -0800

    Create AnotherSampleFixture.php

---
+++ Test/Fixture/AnotherSampleFixture.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * AnotherSampleFixture
+ *
+ */
+class AnotherSampleFixture extends CakeTestFixture {
+
+/**
+ * Import
+ *
+ * @var array
+ */
+  public $import = array('records' => false);
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
+		'title' => array('type' => 'string', 'null' => false, 'default' => null),
+		'message' => array('type' => 'text', 'null' => false, 'default' => null),
+		'user_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10),
+		'created' => array('type' => 'timestamp', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+}
