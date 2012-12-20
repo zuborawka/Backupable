443e92618e87eefd6ae7da325c10555f21178586
Satoshi Murai <zvg03134@nifty.ne.jp>
2012-12-21 00:38:44 +0900

    Create BackupableHelperTest

---
+++ Test/Case/View/Helper/BackupableHelperTest.php
@@ -0,0 +1,50 @@
+<?php
+
+
+App::uses('View', 'View');
+App::uses('Controller', 'Controller');
+App::uses('CakeRequest', 'Network');
+App::uses('CakeResponse', 'Network');
+App::uses('AppModel', 'Model');
+App::uses('AppController', 'Controller');
+App::uses('BackupableHelper', 'Backupable.View/Helper');
+/**
+ * BackupableHelperTest class
+ * 
+ * @package	Backupable.Test.Case.View.Helper
+ */
+class BackupableHelperTest extends CakeTestCase {
+
+	public function setUp() {
+		parent::setUp();
+		$this->Controller = new SamplesController(new CakeRequest(), new CakeResponse());
+		$this->View = new View($this->Controller);
+		$this->Helper = new BackupableHelper($this->View);
+	}
+
+	public function tearDown() {
+		unset($this->Controller, $this->View, $this->Helper);
+	}
+
+	public function testHistory() {
+
+	}
+
+	public function testRemember() {
+	}
+
+
+
+}
+
+class Sample extends AppModel {
+
+	public $actsAs = array('Backupable.Backupable');
+
+}
+
+class SamplesController extends AppController {
+
+	public $helpers = array('Backupable.Backupable');
+
+}
